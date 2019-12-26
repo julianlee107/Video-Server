57d9c6f60b1973746bbd65de3816cd10b42c75b9
李俊霖 <julianlee107@hotmail.com>
2019-12-26 10:06:47 +0800

    做了点小测试

--- api/main.go
+++ api/main.go
@@ -1,1 +1,23 @@
-package api
+package main
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func Handler() *httprouter.Router {
+	router := httprouter.New()
+	router.POST("/", CreateUser)
+	return router
+
+}
+
+func main() {
+	//http.HandleFunc("/",RegisterHandler)
+	r := Handler()
+	e := http.ListenAndServe(":8000", r)
+	if e != nil {
+		log.Fatal(e)
+	}
+}
