cb2da07ab0416282a0109d289ad3b7c6151b7359
李俊霖 <julianlee107@hotmail.com>
2020-01-02 14:44:39 +0800

    新增sessionid生成

--- api/utils/UUID.go
+++ api/utils/UUID.go
@@ -1,1 +1,17 @@
 package utils
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+)
+
+func NewUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		panic(err.Error())
+		return "", err
+	}
+	return fmt.Sprintf("%x",uuid[:]),nil
+}
