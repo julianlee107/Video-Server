40f9062c0df85f5b92ce1c7c25984a6c077417ef
李俊霖 <julianlee107@hotmail.com>
2020-01-02 14:52:16 +0800

    判断是否session是否过期

--- api/session/sessionops.go
+++ api/session/sessionops.go
@@ -3,6 +3,7 @@ package session
 import (
 	"Video-Server/api/dbops"
 	"Video-Server/api/defs"
+	"Video-Server/api/utils"
 	"log"
 	"sync"
 	"time"
@@ -10,35 +11,62 @@ import (
 
 var sessionMap *sync.Map
 
-func init(){
+func init() {
 	sessionMap = &sync.Map{}
 }
 
-func nowInMillSecond() int64{
-	return time.Now().UnixNano()/1000000
+func nowInMillSecond() int64 {
+	return time.Now().UnixNano() / 1000000
 }
 
-func deleteExpireSession(sid string){
+func deleteExpireSession(sid string) {
 	sessionMap.Delete(sid)
 	err := dbops.DeleteSessionById(sid)
-	if err !=nil{
+	if err != nil {
 		log.Printf("%s\n", err)
 	}
 }
 
-func LoadSessionFromDB(){
-	r,err := dbops.RetrieveAllSession()
-	if err != nil{
+func LoadSessionFromDB() {
+	r, err := dbops.RetrieveAllSession()
+	if err != nil {
 		log.Printf("%s\n", err)
 		return
 	}
 	r.Range(func(key, value interface{}) bool {
 		simpleSession := value.(*defs.SimpleSession)
-		sessionMap.Store(key,simpleSession)
+		sessionMap.Store(key, simpleSession)
 		return true
 	})
 }
 
-func GenerateNewSessionId(username string)string  {
+func GenerateNewSessionId(username string) string {
+	id, err := utils.NewUUID()
+	if err != nil {
+		log.Printf("%s\n", err)
+		return ""
+	}
+	currentTime := nowInMillSecond()
+	ttl := currentTime + 30*60*1000
+	simpleSession := &defs.SimpleSession{Username: username, TTL: ttl}
+	sessionMap.Store(id, simpleSession)
+	err = dbops.InsertSession(id, ttl, username)
+	if err != nil {
+		log.Printf("%s\n", err)
+		return ""
+	}
+	return id
+}
 
+func IsSessionIdExpired(sid string) (string, bool) {
+	simpleSession, ok := sessionMap.Load(sid)
+	if ok {
+		ct := nowInMillSecond()
+		if simpleSession.(*defs.SimpleSession).TTL < ct {
+			deleteExpireSession(sid)
+			return "", true
+		}
+		return simpleSession.(*defs.SimpleSession).Username, false
+	}
+	return "", true
 }
